83d8a36e89e815f332b75955c5d22dadf4d7fe0c
Rasul Shagiev <dekuxes@gmail.com>
2022-06-25 13:54:14 +0300

    add pagination

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -7,7 +7,7 @@ const User = ({ user, onDelete, onBookmark }) => {
     <tr key={user._id}>
       <td>{user.name}</td>
       <td><Quality user={user} /></td>
-      <td key={user.profession._id}>{user.profession.name}</td>
+      <td>{user.profession.name}</td>
       <td>{user.completedMeetings}</td>
       <td>{user.rate}/5</td>
       <td><Bookmark user={user} onBookmark={onBookmark} /></td>
--- src/components/usersList.jsx
+++ src/components/usersList.jsx
@@ -1,31 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
+import { paginate } from "../utils/paginate"
+import Pagination from "./pagination"
 import User from "./user"
 
 const UsersList = ({ users, onDelete, onBookmark }) => {
+  const count = users.length
+  const pageSize = 4
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex)
+  }
+
+  const userCrop = paginate(users, currentPage, pageSize)
+
   return (
-    users.length > 0 &&
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качества</th>
-          <th scope="col">Профессия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-          <th scope="col">Избранное</th>
-        </tr>
-      </thead>
-      <tbody className="table-group-divider">
-        {users.map((user) => (
-          <User
-            key={user._id}
-            user={user}
-            onDelete={onDelete}
-            onBookmark={onBookmark}
-          />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {count > 0 &&
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+            </tr>
+          </thead>
+          <tbody className="table-group-divider">
+            {userCrop.map((user) => (
+              <User
+                key={user._id}
+                user={user}
+                onDelete={onDelete}
+                onBookmark={onBookmark}
+              />
+            ))}
+          </tbody>
+        </table>
+      }
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
   )
 }
 
